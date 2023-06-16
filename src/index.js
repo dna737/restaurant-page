a52a581f8483b4be4d83a1674c6d8e3256719d7f
dna737 <adnanhere27@gmail.com>
2023-06-16 15:21:48 -0500

    create method stubs for displaying tabs

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import "./contactUsPage";
+import "./homePage";
+import "./contactUsPage";
+
+const content = document.querySelector("#content");
+
+const displayHomePage = function () {};
+
+const displayMenuPage = function () {};
+
+const displayContactUsPage = function () {};
+
+const removeCurrentContent = function () {};
+
+const initialLoadPage = (() => {
+  displayHomePage();
+  //TODO: add event listeners for the buttons/"tabs"
+})();
